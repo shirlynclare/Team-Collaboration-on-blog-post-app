46bd8cff9ade4f99aa20462eee2a8a49eb61434d
smartgreendeer <168085117+smartgreendeer@users.noreply.github.com>
2024-05-09 08:02:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const togglePassword = document.getElementById("toggle-password");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", function() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+});
+
+const form = document.getElementById("my-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  // Validate the form fields
+  if (name === "" || email === "") {
+    alert("Please fill in all the fields.");
+    return;
+  }
+
+  // Open the new HTML file in the same window
+  window.location.href = "/signin.html?name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email);
+});
+
